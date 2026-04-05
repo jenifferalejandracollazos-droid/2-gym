68357bdd25aea0f1e60dac809a169da79c92dc87
Jeniffer Collazos <jecollazosdev@gmail.com>
2026-04-05 00:20:06 -0500

    Fix wsgi.py import for Render

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,7 +1,9 @@
-# This file was created to run the application on heroku using gunicorn.
-# Read more about it here: https://devcenter.heroku.com/articles/python-gunicorn
+import sys
+import os
 
-from app import app as application
+sys.path.insert(0, os.pathdirname(__file__))
+
+from app import app
 
 if __name__ == "__main__":
-    application.run()
+    app.run()   
